6eb462b9edd061d20577a21b77ef30186eb4cbb9
Manuel Seelaus <mseelaus@atti.com>
2011-10-26 16:38:21 -0400

    added sync scripts

---
+++ bin/synchome.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+running=`ps -u mseelaus | grep synchome.sh | grep -v grep | wc -l`
+echo $running
+if [ $running -lt 3 ]; then
+    while [ 1 ]; do
+        cd
+        echo `date` >> .synclog
+#        git add . 2>&1 >> .synclog
+        git commit -a -m "auto upload" 2>&1 >> .synclog
+        git pull 2>&1 >> .synclog
+        git push origin master 2>&1 >> .synclog
+        sleep 600
+    done
+else
+    echo "synchome already running"
+fi
---
+++ bin/syncinit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash        
+set -ue
+
+if [ `whoami` != 'mseelaus' ]; then
+    echo 'please run this script as user mseelaus'
+    exit 1
+else
+    cd
+    if [ -d '.git' ]; then
+        echo 'init was already run.'
+        exit 1
+    else
+        echo 'go'
+        # get keys from manubuntu
+# TODO: get from another server (ssvm)
+        scp -r mseelaus.np.wc1.yellowpages.com:~/.ssh .
+        chmod 0600 .ssh/*
+# TODO: check if git is installed
+        git clone git@git.corp.attinteractive.com:mseelaus/home.git
+        rsync -a home/ .
+        rm -rf home
+        chmod 0600 .ssh/*
+        echo '# periodically upload file changes to git' >> .bashrc
+        echo '~/bin/synchome.sh 2>&1 >> .synclog &' >> .bashrc
+    fi
+fi         
